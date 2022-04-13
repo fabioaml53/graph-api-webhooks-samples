49d3cc76689d8bf7f9e691ff9da75869f500e87d
fabioaml53 <103372806+fabioaml53@users.noreply.github.com>
2022-04-13 15:05:14 -0300

    assda

--- heroku/index.js
+++ heroku/index.js
@@ -32,25 +32,7 @@ app.get(['/facebook2'], function(req, res) {
    console.log("-= OK =- ");
 
   
-  http.get('http://[2804:10b4:117:4800:c97d:301a:251a:b9b1]:8080/api/v1/posts/100080354891590/10-01-2022/20-04-2022', (resp) => {
- console.log('STATUS: ' + resp.statusCode);
-  resp.on("data", function(chunk) {
-    console.log("BODY: " + chunk);
-	
-	   
-    objectValue = JSON.parse(chunk);
-    console.log(objectValue);   	
-	
-	
-	res.send("<a href='"+objectValue['link_solicitacao']+"'>"  +" OK </a>");
-	
-  });
   
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-  
-
 });
 
 
